0ee71b6413ff8b2d523b5300703aade55d1473a1
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-03-11 21:55:06 +0100

    added '/api'

--- api-client/src/main/java/org/example/Main.java
+++ api-client/src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Main {
 
-    public static String apiBasePath = "http://localhost:8080";
+    public static String apiBasePath = "http://localhost:8080/api";
 
     public static boolean clientRun = true;
     public static void main(String[] args) {
